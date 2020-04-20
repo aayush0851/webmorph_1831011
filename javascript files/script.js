b7200045b081e2d5b1c7ecb10c68d3cdd8c436b5
aayush0851 <tyagiayush851@gmail.com>
2020-04-20 21:46:37 +0530

    JavaScript functionalities added to page 1

--- javascript files/script.js
+++ javascript files/script.js
@@ -1,1 +1,36 @@
-document.getElementsByTagName('body').contentEditable='false';
+var counter = false;
+function toggle(){
+    if(counter===false){
+        document.getElementById('toggle-bar').style.height="100vh";
+        document.getElementById('togList').style.display="block";
+        document.getElementById('close').style.display="block";
+        counter=true;
+    }
+    else{
+        document.getElementById('toggle-bar').style.height="0vh";
+        document.getElementById('togList').style.display="none";
+        document.getElementById('close').style.display="none";
+        counter=false;
+        searchBox();
+    }
+}
+
+var magCount = false;
+
+function searchBox(){
+    if(magCount===false){
+        arrClass[0].style.display="block";
+        magCount=true;
+    }
+    else{
+        arrClass[0].style.display="none";
+        magCount=false;
+    }
+}
+
+document.getElementById('hamBurger').addEventListener("click", toggle);
+document.getElementById('close').addEventListener("click", toggle);
+document.getElementById('magnify').addEventListener("click", searchBox);
+var arrClass = document.getElementsByClassName('hideSearch');
+
+
